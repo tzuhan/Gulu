6be0d0074f05a7877ebc709c4a571ab0ece80ee7
gary9716 <gary9716@gmail.com>
2014-05-01 16:27:51 +0800

    keep arduino code to date

--- smartCup_ColorPAL/smartCup_ColorPAL.ino
+++ smartCup_ColorPAL/smartCup_ColorPAL.ino
@@ -1,29 +1,23 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-//#include "TCS3200.h"
 #include "HX711.h"
-//#include "ColorPAL.h"
 
 const long bluetoothBaudRate = 57600;
 
 const byte HX711_DT = A1;
 const byte HX711_SCK = A0;
 
+const int triggerPin = 11;
+
 const int sio = 10;
 const int unused = 255; 		// Non-existant pin # for SoftwareSerial
 const int sioBaud = 4800;
-const int waitDelay = 200;
-const int sampleNum = 10;
-int sampleTurn = sampleNum;
+const int waitDelay = 100;
+const int colorSampleNum = 15;
+const int weightSampleNum = 30;
+int sampleTurn = colorSampleNum;
 
-//ColorPAL colorSensor(ColorPAL_sio,waitDelay);
-/*
-TCS3200 colorSensor(
-TCS3200_S0,TCS3200_S1,
-TCS3200_S2,TCS3200_S3,
-TCS3200_taosOutPin,TCS3200_LED);
-*/
 HX711 scale(HX711_DT,HX711_SCK);
 
 SoftwareSerial serin(sio, unused);
@@ -32,6 +26,11 @@ SoftwareSerial serout(unused, sio);
 void setup() {
   //Serial.begin(bluetoothBaudRate);
   Serial.begin(9600);
+  
+  // open reading == HIGH
+  // closed reading == LOW
+  pinMode(triggerPin, INPUT_PULLUP);
+  
   reset();				  // Send reset to ColorPal
   serout.begin(sioBaud);
   pinMode(sio, OUTPUT);
@@ -39,8 +38,8 @@ void setup() {
   serout.end();			  // Discontinue serial port for transmitting
 
   serin.begin(sioBaud);	        // Set up serial port for receiving
-  pinMode(sio, INPUT);
-  
+  pinMode(sio, INPUT); 
+    
   //scale.set_scale(scale.get_units(10)/50);  // this value is obtained by calibrating the scale with known weights; see the README for details
   //scale.tare();
   
@@ -50,21 +49,68 @@ void setup() {
   
 }
 
+int previousWeight = 0;
+int currentWeight = 0;
+const int WeightNoise = 5;
+
 void loop() {
+  if(digitalRead(triggerPin) == LOW) { //closed circuit
   
+    scale.power_up();
+    
+    currentWeight = scale.get_units(weightSampleNum);
+    if(previousWeight == 0) {
+      //ColorData
+      readDataAndSample();
+    }
+    else {
+      while(  abs(previousWeight - currentWeight) < WeightNoise //not noise
+              || previousWeight < currentWeight 
+              || (abs(currentWeight) > WeightNoise && currentWeight < 0) ) {      
+           currentWeight = scale.get_units(weightSampleNum); //resample
+      }
+    }
+    
+    if(abs(currentWeight) < WeightNoise) { //empty
+      previousWeight = 0; //initialize
+      Serial.println("empty");
+    }
+    else {
+      previousWeight = currentWeight;
+      Serial.print("weight:");
+      Serial.println(currentWeight);
+    }
+    
+    scale.power_down();// put the ADC in sleep mode
+  
+  }
+}
+
+int red[colorSampleNum] = {0};
+int green[colorSampleNum] = {0};
+int blue[colorSampleNum] = {0};
+
+void readDataAndSample() {
+  sampleTurn = colorSampleNum;
   while(sampleTurn){
     readData();
   }
-  sampleTurn = sampleNum;
   
-  /*
-  Serial.print("avg_weight:");
-  Serial.println(scale.get_units(10));
-  Serial.println("");
-  scale.power_down();			        // put the ADC in sleep mode
-  delay(10);
-  scale.power_up();
-  */
+  Serial.print("RGB:");
+  Serial.print(getAverage(red));
+  Serial.print(" ");
+  Serial.print(getAverage(green));
+  Serial.print(" ");
+  Serial.println(getAverage(blue));
+  
+}
+
+float getAverage(int *values) {
+  long sum = 0;
+  for(int i=0;i < colorSampleNum;i++) {
+    sum+=values[i];
+  }
+  return ((float)sum)/colorSampleNum;
 }
 
 // Reset ColorPAL; see ColorPAL documentation for sequence
@@ -82,22 +128,30 @@ void reset() {
 }
 
 void readData() {
-  char buffer[32];
+  char buffer[32] = {0};
   
   if (serin.available() > 0) {
     // Wait for a $ character, then read three 3 digit hex numbers
-    buffer[0] = serin.read();
-    if (buffer[0] == '$') {
+    if (serin.read() == '$') {
       for(int i = 0; i < 9; i++) {
         while (serin.available() == 0);     // Wait for next input character
         buffer[i] = serin.read();
         if (buffer[i] == '$')               // Return early if $ character encountered
           return;
       }
-      parseAndPrint(buffer);
+      parseAndCollectData(buffer);
       delay(10);
     }
   }
+}
+
+void parseAndCollectData(char *data) {
+  sampleTurn--;
+  sscanf (data, "%3x%3x%3x"
+  , red + sampleTurn
+  , green + sampleTurn
+  , blue + sampleTurn);
+  
 }
 
 // Parse the hex data into integers
